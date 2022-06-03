2762eca456a0d63d942deeec9774eb0defc0a7ec
SmartCodeBlockchainDev <kapobear9509@gmail.com>
2022-06-03 21:22:26 +0300

    AAVE interface connection

--- contracts/FatimaLending.sol
+++ contracts/FatimaLending.sol
@@ -5,26 +5,26 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import {ILendingPool} from './ILendingPool.sol';
+
 contract FatimaLending is Ownable {
     IERC20 internal len_token;
-    address internal aave_address;
+    ILendingPool internal len_pool;
 
     constructor(address _aave) {
-        aave_address = _aave;
+        len_pool = ILendingPool(_aave);
         console.log("Fatima Lending Platform");
     }
 
-    function setting(address _address) public onlyOwner {
-        aave_address = _address;
+    function setLendingPool(address _aave) public onlyOwner {
+        len_pool = ILendingPool(_aave);
     }
 
     function deposit(address _address, uint256 _amount) public {
-        len_token = IERC20(_address);
-        len_token.transferFrom(msg.sender, aave_address, _amount);
+
     }
 
     function withdraw(address _address, uint256 _amount) public {
-        len_token = IERC20(_address);
-        len_token.transferFrom(aave_address, msg.sender,_amount);
+        
     }
 }
