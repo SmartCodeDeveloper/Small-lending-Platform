ee7cf646a2e57cf3be562cfb6c3ed3614ebebe2c
SmartCodeBlockchainDev <kapobear9509@gmail.com>
2022-06-03 09:07:54 +0300

    Skeleton

---
+++ contracts/FatimaLending.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract FatimaLending {
+
+    constructor() {
+        console.log("Fatima Lending Platform");
+    }
+
+    function deposit(address _address, uint256 _amount) public {
+
+    }
+
+    function withdraw(address _address, uint256 _amount) public {
+
+    }
+}
